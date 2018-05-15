448c20b10161783bd146fef20b8d1cef36308d3b
Alcides Dias <32815904+Arushidesu@users.noreply.github.com>
2018-05-15 19:24:15 -0300

    Create 1564.py

---
+++ 1564.py
@@ -0,0 +1,9 @@
+while True:
+    try:
+        N = int(input())
+        if N > 0:
+            print("vai ter duas!")
+        else:
+            print("vai ter copa!")
+    except EOFError:
+        break
