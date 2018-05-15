ef77893f08b9c47a4cdadf37d0600fac682f8a6c
Alcides Dias <32815904+Arushidesu@users.noreply.github.com>
2018-05-15 19:29:39 -0300

    Create 1864.py

---
+++ 1864.py
@@ -0,0 +1,7 @@
+soren = ['L', 'I', 'F', 'E', ' ', 'I', 'S', ' ', 'N', 'O', 'T', ' ', 'A', ' ', 'P', 'R', 'O', 'B', 'L', 'E', 'M',
+         ' ', 'T', 'O', ' ', 'B', 'E', ' ', 'S', 'O', 'L', 'V', 'E', 'D']
+N = int(input())
+soma = ''
+for i in range(N):
+    print(soren[i], end='')
+print()
