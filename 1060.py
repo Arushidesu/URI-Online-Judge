228e5b722fedc1378dd4aa5db930e1c4572adcdc
Alcides Dias <32815904+Arushidesu@users.noreply.github.com>
2018-05-15 18:54:13 -0300

    Create 1060.py

---
+++ 1060.py
@@ -0,0 +1,20 @@
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+e = float(input())
+f = float(input())
+aux = 0
+if a > 0:
+    aux += 1
+if b > 0:
+    aux += 1
+if c > 0:
+    aux += 1
+if d > 0:
+    aux += 1
+if e > 0:
+    aux += 1
+if f > 0:
+    aux += 1
+print("%d valores positivos" %aux)
