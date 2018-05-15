58ee8fd27405641f905a821fed81962fb5a49ce9
Alcides Dias <32815904+Arushidesu@users.noreply.github.com>
2018-05-15 19:19:26 -0300

    Create 1183.py

---
+++ 1183.py
@@ -0,0 +1,23 @@
+O = input()
+matriz = []
+soma = media = 0
+cont = 1
+for i in range(12):
+   tmp = []
+   for j in range(12):
+       elemento = float(input())
+       tmp.append(elemento)
+   matriz.append(tmp[:])
+if O == "S":
+    for i in range(11):
+        for j in range(cont, 12):
+            soma = matriz[i][j] + soma
+        cont += 1
+    print("%.1f" % (soma))
+elif O == "M":
+    for i in range(11):
+        for j in range(cont, 12):
+            soma = matriz[i][j] + soma
+        cont += 1
+    media = "%.1f" % (soma / 66)
+    print(media)
